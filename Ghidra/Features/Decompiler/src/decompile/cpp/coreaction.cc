a9c4f8ff7e6cab36648902ecc4fdcfc591a1c669
AltimorTASDK <altimormc@gmail.com>
2022-07-22 12:22:41 -0400

    disable RuleShiftAnd

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -5135,7 +5135,7 @@ void ActionDatabase::universalAction(Architecture *conf)
 				actprop->addRule( new RuleConcatZext("analysis") );
 				actprop->addRule( new RuleZextCommute("analysis") );
 				actprop->addRule( new RuleZextShiftZext("analysis") );
-				actprop->addRule( new RuleShiftAnd("analysis") );
+				//actprop->addRule( new RuleShiftAnd("analysis") );
 				actprop->addRule( new RuleConcatZero("analysis") );
 				actprop->addRule( new RuleConcatLeftShift("analysis") );
 				actprop->addRule( new RuleEmbed("analysis") );
